403b3b0367fa05afd4f54c8463626540ec4242e6
Vipin kumar <vipinkumarvipinkumar20@gmail.com>
2023-05-12 11:59:49 +0530

    Initial Project Setup

---
+++ ShopingApp_git/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopingApp_git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
